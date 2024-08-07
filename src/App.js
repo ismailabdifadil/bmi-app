3a8a0f24e2b407fe88f02b8c72021aef97cc17d9
Ismail <ismail.haabare@gmail.com>
2024-08-07 11:29:20 +0300

    G

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <>
       <div className=" w-full h-lvh bg-primary">
         <div className="container flex flex-col justify-between  py-[40px] text-center px-8 font-roboto">
-       git    <h1 className=" text-4xl text-white font-bold mb-2">BMiDO</h1>
+          <h1 className=" text-4xl text-white font-bold mb-2">BMiDO</h1>
           <img src={logo} alt="log" />
           <div className="px-4 py-3">
             <h2 className="text-white text-xl font-bold text-left pb-4">
