526abc4f92db7b1affc9b6ce7ed2397d23debf6f
Ismail <ismail.haabare@gmail.com>
2024-08-11 12:49:28 +0300

    Added routes

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+const WelcomePage = () => {
+  return (
+    <>
+      <div className=" w-full h-lvh bg-primary">
+        <div className="container flex flex-col justify-between  py-[40px] text-center px-3 font-roboto">
+          <h1 className=" text-4xl text-white font-bold mb-2">BMiDO</h1>
+          <img src={logo} alt="log" />
+          <div className="px-4 py-3">
+            <h2 className="text-white text-xl font-bold text-left pb-4">
+              Get Started with Tracking Your Health!
+            </h2>
+            <p className="text-[#C6C3F9] text-left text-xs leading-tight tracking-widest font-semibold">
+              Calculate your BMI and stay on top of your wellness journey,
+              effortlessly.
+            </p>
+          </div>
+          {/* </div>  */}
+          <Link to={"/home"}>
+            <button className="btn btn-secondary">Get Started</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WelcomePage;
