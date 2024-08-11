d51be51c807c9458b1a126053c3c3dd7da66ccf4
Ismail <ismail.haabare@gmail.com>
2024-08-11 12:48:58 +0300

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './assets/logo.png';
+import { Outlet } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
 function App() {
   return (
     <>
-      <div className=" w-full h-lvh bg-primary">
-        <div className="container flex flex-col justify-between  py-[40px] text-center px-8 font-roboto">
-          <h1 className=" text-4xl text-white font-bold mb-2">BMiDO</h1>
-          <img src={logo} alt="log" />
-          <div className="px-4 py-3">
-            <h2 className="text-white text-xl font-bold text-left pb-4">
-              Get Started with Tracking Your Health!
-            </h2>
-            <p className="text-[#C6C3F9] text-left text-xs leading-tight tracking-widest font-semibold">
-              Calculate your BMI and stay on top of your wellness journey,
-              effortlessly.
-            </p>
-          </div>
-          {/* </div>  */}
-          <button className="btn btn-secondary">Get Started</button>
-        </div>
-      </div>
+      <Outlet />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <WelcomePage />,
+      },
+    ],
+  },
+  {
+    path: '/home',
+    element: <HomePage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
